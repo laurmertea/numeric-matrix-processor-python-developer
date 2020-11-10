e237ad5bd03736e9791ce4ca4dec3a769da8dd48
laurmertea <mertealaur@yahoo.com>
2020-11-10 02:26:37 +0100

    Add first stage script

--- scripts/addition.py
+++ scripts/addition.py
@@ -51,3 +51,76 @@
 # 1 3 4 3 8
 # Output:
 # ERROR
+
+class Matrix:
+    """Create a numeric matrix.
+    
+    Arguments:
+        row_dim -- The number of matrix rows
+        col_dim -- The number of matrix columns
+
+    Keyword arguments:
+        rows -- The matrix rows (if not passed, they will be read from input)
+    """
+    def __init__(self, row_dim, col_dim, rows=None):
+        self.row_dim = row_dim
+        self.col_dim = col_dim
+        self.rows = []
+
+        if not rows:
+            self.get_rows()
+        else:
+            self.rows = rows
+        
+    def get_rows(self):
+        """Get the matrix rows from input."""
+        index = 0
+
+        while index < self.row_dim:
+            self.rows.append(list(map(int,input().split())))
+            index = index + 1
+
+    def repr(self, repr_type=None):
+        """Print a representation of the matrix.
+        
+        Keyword arguments:
+            repr_type -- The matrix representation type; 
+                         if None is passed then the matrix will be printed as an array of arrays
+        """
+        if not repr_type:
+            print(self.rows)
+        else:
+            for i in range(0, self.row_dim):
+                print(' '.join([str(x) for x in self.rows[i]]))
+
+
+def mat_addition(a, b):
+    """Add two matrices.
+
+    Arguments:
+        a -- The first matrix
+        b -- The second matrix
+
+    Returns:
+        A matrix if the addition could be performed, or None
+    """
+    if a.row_dim != b.row_dim or a.col_dim != b.col_dim:
+        return None
+    rows = []
+    for i in range(0, a.row_dim):
+        cols = []
+        for j in range(0, a.col_dim):
+            cols.append(a.rows[i][j] + b.rows[i][j])
+        rows.append(cols)
+    return Matrix(a.row_dim, a.col_dim, rows)
+
+x, y = map(int,input().split())
+mat_A = Matrix(x, y)
+x, y = map(int,input().split())
+mat_B = Matrix(x, y)
+mat_C = mat_addition(mat_A, mat_B)
+
+if mat_C:
+    mat_C.repr("table")
+else:
+    print("ERROR")
