f457816f9b23e2bbc781134e390e71413383a54f
laurmertea <mertealaur@yahoo.com>
2020-11-21 04:59:04 +0100

    Add menu. Add transpose. Add matrices multiplication

--- scripts/processor.py
+++ scripts/processor.py
@@ -1,3 +1,85 @@
+import sys
+
+
+# MENU SETUP
+menu_options = ['1. Add matrices', '2. Multiply matrix by a constant', '3. Multiply matrices', '0. Exit']
+selected_option = None
+
+
+# MESSAGES SETUP
+impossible_operation = "The operation cannot be performed.\n"
+matrix_size_input = "Enter size of matrix: "
+first_matrix_size_input = "Enter size of first matrix: "
+second_matrix_size_input = "Enter size of second matrix: "
+matrix_input = "Enter matrix:"
+first_matrix_input = "Enter first matrix:"
+second_matrix_input = "Enter second matrix:"
+constant_input = "Enter constant: "
+result_output = "The result is:"
+unsupported_option = "Not supported!"
+
+
+def exit_proc(message=""):
+    """Exit with optional message.
+    
+    Keyword arguments:
+        message -- string to exit with as message (default is an empty string)
+    """
+    sys.exit(message)
+
+
+def processor_menu(selected):
+    """Try and access selected option in the processor menu (see the `menu_options` list).
+
+    Arguments:
+        selected -- a valid integer matching an option from menu_options list
+    """
+    if selected_option == 0:
+        exit_proc()
+    else:
+        if selected_option == 1:
+            x, y = map(int,input(first_matrix_size_input).split())
+            print(first_matrix_input)
+            mat_A = Matrix(x, y)
+            x, y = map(int,input(second_matrix_size_input).split())
+            print(second_matrix_input)
+            mat_B = Matrix(x, y)
+
+            mat_C = mat_addition(mat_A, mat_B)
+            if mat_C:
+                print(result_output)
+                mat_C.repr("table")
+            else:
+                print(impossible_operation)            
+        elif selected_option == 2:
+            x, y = map(int,input(matrix_size_input).split())
+            print(matrix_input)
+            mat_A = Matrix(x, y)
+            multiplier = float(input(constant_input))
+            mat_B = mat_multiply_constant(mat_A, multiplier)
+            if mat_B:
+                print(result_output)
+                mat_B.repr("table")
+            else:
+               print(impossible_operation) 
+        elif selected_option == 3:
+            x, y = map(int,input(first_matrix_size_input).split())
+            print(first_matrix_input)
+            mat_A = Matrix(x, y)
+            x, y = map(int,input(second_matrix_size_input).split())
+            print(second_matrix_input)
+            mat_B = Matrix(x, y)
+
+            mat_C = mat_multiply(mat_A, mat_B)
+            if mat_C:
+                print(result_output)
+                mat_C.repr("table")
+            else:
+                print(impossible_operation)   
+        else:
+            print(unsupported_option)
+
+
 class Matrix:
     """Create a numeric matrix.
     
@@ -23,7 +105,7 @@ class Matrix:
         index = 0
 
         while index < self.row_dim:
-            self.rows.append(list(map(int,input().split())))
+            self.rows.append(list(map(float,input().split())))
             index = index + 1
 
     def repr(self, repr_type=None):
@@ -80,19 +162,51 @@ def mat_multiply_constant(mat, const):
     return Matrix(mat.row_dim, mat.col_dim, rows)
 
 
-x, y = map(int,input().split())
-mat_A = Matrix(x, y)
-x, y = map(int,input().split())
-mat_B = Matrix(x, y)
+def mat_transpose(mat):
+    """Transpose a matrix.
 
-mat_C = mat_addition(mat_A, mat_B)
-if mat_C:
-    mat_C.repr("table")
-else:
-    print("ERROR")
+    Arguments:
+        mat -- The matrix
 
-x, y = map(int,input().split())
-mat_D = Matrix(x, y)
-multiplier = int(input())
-mat_E = mat_multiply_constant(mat_D, multiplier)
-mat_E.repr("table")
+    Returns:
+        A matrix
+    """
+    rows = []
+    for j in range(0, mat.col_dim):
+        cols = []
+        for i in range(0, mat.row_dim):
+            cols.append(mat.rows[i][j])
+        rows.append(cols)
+    return Matrix(mat.col_dim, mat.row_dim, rows)
+
+
+def mat_multiply(mat_a, mat_b):
+    """Multiply two matrices.
+
+    Arguments:
+        mat_a -- The first matrix
+        mat_b -- The second matrix
+
+    Returns:
+        A matrix if the multiplication could be performed, or None
+    """
+    if mat_a.col_dim != mat_b.row_dim:
+        return None
+    
+    mat_bt = mat_transpose(mat_b)
+    rows = []
+    for i in range(0, mat_a.row_dim):
+        cols = []
+        for j in range(0, mat_bt.row_dim):
+            result = 0
+            for k in range(0, mat_a.col_dim):
+                result += mat_a.rows[i][k] * mat_bt.rows[j][k]
+            cols.append(result)
+        rows.append(cols)
+    return Matrix(mat_a.row_dim, mat_b.col_dim, rows)
+
+
+while selected_option not in range(0, 3):
+    while selected_option != 0:
+        selected_option = int(input('\n'.join(menu_options) + '\n' + 'Your choice: '))
+        processor_menu(selected_option)
